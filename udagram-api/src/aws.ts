41f9cf4043d63dffd53bd85dd12b4e7489358d7b
SultanMohammedd <sult10.sa@gmail.com>
2023-02-14 11:10:22 +0300

    init

--- udagram-api/src/aws.ts
+++ udagram-api/src/aws.ts
@@ -3,7 +3,7 @@ import { config } from "./config/config";
 
 // Configure AWS
 //const credentials = new AWS.SharedIniFileCredentials({ profile: "default" });
-
+//
 const credentials = new AWS.Credentials({
   accessKeyId: config.aws_access_key,
   secretAccessKey: config.aws_secret,
